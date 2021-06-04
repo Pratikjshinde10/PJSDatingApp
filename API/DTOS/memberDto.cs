c03335f807286462b124d352e1f280509c5d533f
pratikjshinde10@gmail.com <pratikjshinde10@gmail.com>
2021-06-04 17:06:38 +0530

    End of Section 8

--- API/DTOS/memberDto.cs
+++ API/DTOS/memberDto.cs
@@ -1,17 +1,15 @@
 using System;
 using System.Collections.Generic;
-using API.Extensions;
+using API.Entities;
 
-namespace API.Entities
+namespace API.DTOS
 {
-    public class AppUser
+    public class memberDto
     {
         public int Id { get; set; }
         public string UserName { get; set; }
-
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public string PhotoUrl { get; set; }
+        public int Age{ get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; } = DateTime.Now;
         public DateTime LastActive { get; set; } = DateTime.Now;
@@ -21,7 +19,6 @@ namespace API.Entities
         public string Interests { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
-        public ICollection<Photo> Photos { get; set; }
-     
+        public ICollection<PhotoDto> Photos { get; set; }
     }
 }
