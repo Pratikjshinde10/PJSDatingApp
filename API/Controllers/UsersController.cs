208b1c4e9788dc9d64072be0462f29f846d70378
pratikjshinde10@gmail.com <pratikjshinde10@gmail.com>
2021-07-05 21:01:25 +0530

    End of Section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOS;
@@ -32,6 +33,7 @@ namespace API.Controllers
             // return Ok(usersToReturn);
 
             var users = await _userRepository.GetMembersAsync();
+            
             return Ok(users);
         } 
 
@@ -43,5 +45,20 @@ namespace API.Controllers
 
             return await _userRepository.GetMemberAsync(username);
         } 
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRepository.Update(user);
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
+        }
     }
 }
--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 
@@ -11,15 +12,32 @@ import { Member } from '../_models/member';
 })
 export class MembersService {
   baseUrl = environment.apiURL;
+  members: Member[] = [];
 
   constructor(private http: HttpClient) { }
 
   getMembers(): Observable<Member[]> {
-    return this.http.get<Member[]>(this.baseUrl + 'users');
+    if (this.members.length > 0) { return of(this.members); }
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
+      map(members => {
+        this.members = members;
+        return members;
+      })
+    );
   }
 
   getMember(username): Observable<Member> {
-    return this.http.get<Member>(this.baseUrl + 'users/' + username);
+    const member = this.members.find(x => x.userName === username);
+    if (member !== undefined) { return of(member); }
   }
 
+  // tslint:disable-next-line:typedef
+  updateMember(member: Member) {
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = member;
+      })
+    );
+  }
 }
